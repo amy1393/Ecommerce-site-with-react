7913b3f1e2fe7c166f3ea9efe57492dea7e5cdfe
amy <amityadav1393@gmail.com>
2020-09-27 17:57:28 +0530

    added cart componentcart-dropdown,cart-reducer,cart.action & cart.utils to group cartitems

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import {connect} from 'react-redux';
@@ -48,15 +48,21 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={SignInAndSignUpPage} />
+          <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to = '/' />): (<SignInAndSignUpPage />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user}) =>(
+  {
+    currentUser: user.currentUser
+  }
+)
+
 const mapDispatchToProps = dispatch =>({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import './collection-item.component.styles.scss'
+import React from 'react';
+import './collection-item.component.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
 
-const CollectionItem = ({id,imageUrl,name,price})=> (
+import {connect} from 'react-redux';
+
+import {addItem} from '../../redux/cart/cart.action'
+
+const CollectionItem = ({item,addItem})=> {
+    const {imageUrl,name,price} = item;
+    return(
     <div className='collection-item'>
 
     <div className='image' style={{backgroundImage: `url(${imageUrl})`}}/>
@@ -11,9 +18,14 @@ const CollectionItem = ({id,imageUrl,name,price})=> (
         <span className='price'>{price}</span>
 
     </div>
+    <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
 
     </div>
 
-)
+)}
 
-export default CollectionItem;
+const mapDispatchToprops = dispatch =>({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null,mapDispatchToprops)(CollectionItem);
